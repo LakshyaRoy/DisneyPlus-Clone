2f8361a2e834df126749147036ddfd0587493b11
lakshya <lakshyaroy848@gmail.com>
2023-07-26 22:18:05 +0530

    added component in home pages with react slick

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,24 +1,44 @@
 // import styled from "styled-components";
 
 import React from "react";
+import { styled } from "styled-components";
+import BackGround from "../images/images/home-background.png";
+import ImageSlider from "./imageSlider";
+import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import DisneyPlus from "./DisneyPlus";
+import Originals from "./Originals";
+import Trending from "./Tranding";
+
+const Container = styled.main`
+  position: relative;
+  background-image: url(${BackGround}) center center / cover no-repeat fixed;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after {
+    background: url(${BackGround}) center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 const Home = () => {
   return (
-    <>
-      <div
-        style={{
-          color: " white",
-          zIndex: " 9",
-          position: " absolute",
-          top: " 50%",
-        }}
-      >
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim rerum
-        repellat cupiditate repudiandae deserunt autem totam dolores! Eius odit
-        rerum nemo facilis ipsam, veritatis culpa libero. Molestiae, quia
-        accusantium!
-      </div>
-    </>
+    <Container>
+      <ImageSlider />
+      <Viewers />
+      <Recommends />
+      <DisneyPlus />
+      <Originals />
+      <Trending />
+    </Container>
   );
 };
 
