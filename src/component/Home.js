dc8a6a35cdda2f7c04fb5a5d024718e405031d12
lakshya <lakshyaroy848@gmail.com>
2023-07-28 01:41:30 +0530

    fetched the data from database

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,6 @@
 // import styled from "styled-components";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import BackGround from "../images/images/home-background.png";
 import ImageSlider from "./imageSlider";
@@ -9,6 +9,16 @@ import Recommends from "./Recommends";
 import DisneyPlus from "./DisneyPlus";
 import Originals from "./Originals";
 import Trending from "./Tranding";
+import { useDispatch, useSelector } from "react-redux";
+import { setMovies } from "../feature/movie/MoviesSlice";
+import { setUserLoginDetails } from "../feature/user/userSlice";
+import db from "../FireBase/Firebase";
+import {
+  QuerySnapshot,
+  collection,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
 
 const Container = styled.main`
   position: relative;
@@ -30,14 +40,60 @@ const Container = styled.main`
 `;
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const [movies, setMovies] = useState({
+    recommends: [],
+    newDisney: [],
+    originals: [],
+    trending: [],
+  });
+
+  const userData = useSelector((state) => state.userSlice.user);
+  useEffect(() => {
+    const q = query(collection(db, "movies"));
+
+    const unsub = onSnapshot(q, (QuerySnapshot) => {
+      const updatedMovies = {
+        recommends: [],
+        newDisney: [],
+        originals: [],
+        trending: [],
+      };
+
+      QuerySnapshot.forEach((doc) => {
+        const movieData = doc.data();
+        switch (movieData.type) {
+          case "recommend":
+            updatedMovies.recommends.push(movieData);
+            break;
+          case "new":
+            updatedMovies.newDisney.push(movieData);
+            break;
+          case "original":
+            updatedMovies.originals.push(movieData);
+            break;
+          case "trending":
+            updatedMovies.trending.push(movieData);
+            break;
+          default:
+            break;
+        }
+      });
+
+      setMovies(updatedMovies); // Update the state with the fetched data
+    });
+
+    return () => unsub(); // Detach the snapshot listener on unmount
+  }, [userData.name]);
+
   return (
     <Container>
       <ImageSlider />
       <Viewers />
-      <Recommends />
-      <DisneyPlus />
-      <Originals />
-      <Trending />
+      <Recommends data={movies.recommends} />
+      <DisneyPlus data={movies.newDisney} />
+      <Originals data={movies.originals} />
+      <Trending data={movies.trending} />
     </Container>
   );
 };
