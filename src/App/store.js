93914e16c1636528a744ee9291f0d6b26ced4298
lakshya <lakshyaroy848@gmail.com>
2023-07-28 01:39:27 +0530

    added movies data in reducer

--- src/App/store.js
+++ src/App/store.js
@@ -1,10 +1,12 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../feature/user/userSlice"; // Correct the import here
+import movieReducer from "../feature/movie/MoviesSlice";
 
 export default configureStore({
   reducer: {
     userSlice,
+    movie: movieReducer,
   },
 
   // middleware: getDefaultMiddleware({
