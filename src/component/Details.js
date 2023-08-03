21dd71d91e7cd0fc3f31aec03df7b6b51f4ea7c0
lakshya <lakshyaroy848@gmail.com>
2023-08-03 22:11:26 +0530

    bug in detailed page yet to be solved

--- src/component/Details.js
+++ src/component/Details.js
@@ -7,6 +7,187 @@ import { useParams } from "react-router-dom";
 import db from "../FireBase/Firebase";
 import { collection, doc, getDoc, onSnapshot, query } from "firebase/firestore";
 
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+`;
+
+const BackGround = styled.div`
+  left: 0px;
+  opacity: 0.8;
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  width: 100%;
+  z-index: -1;
+  img {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+
+    @media screen and (max-width: 768px) {
+      width: initial;
+    }
+  }
+`;
+
+const ImageTitle = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  -webkit-box-pack: start;
+  margin: 0px auto;
+  height: 30vw;
+  min-height: 170px;
+  padding-bottom: 24px;
+  width: 100%;
+
+  img {
+    max-width: 600px;
+    min-width: 200px;
+    width: 35vw;
+  }
+`;
+const ContentMeta = styled.div`
+  max-width: 874px;
+`;
+const Controls = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 24px 0px;
+  min-height: 56px;
+`;
+
+const Player = styled.button`
+  font-size: 15px;
+  padding: 0px 24px;
+  height: 56px;
+  border-radius: 4px;
+  color: rgb(0, 0, 0);
+  border: none;
+  outline: none;
+  cursor: pointer;
+  text-transform: uppercase;
+  background-color: rgba(249, 249, 249);
+  margin: 0px 22px 0px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 1.8;
+  text-align: center;
+
+  img {
+    width: 32px;
+  }
+  &:hover {
+    background-color: rgb(198, 198, 198);
+    transition: 0.2s ease all;
+  }
+  @media screen and (max-width: 768px) {
+    height: 45px;
+    padding: 0px 12px;
+    margin: 0px 10px 0px 0px;
+    font-size: 12px;
+
+    img {
+      width: 25px;
+    }
+  }
+`;
+
+const Trailer = styled(Player)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+  border-radius: 4px;
+  margin-left: 16px;
+  cursor: pointer;
+`;
+const AddList = styled.div`
+  margin-right: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: rgb(249, 249, 249);
+  background: rgba(0, 0, 0, 0.6);
+  font-size: 20px;
+  height: 44px;
+  width: 44px;
+  border-radius: 50%;
+  border: 2px solid rgb(249, 249, 249);
+  cursor: pointer;
+
+  span {
+    background-color: rgba(249, 249, 249);
+    display: inline-block;
+
+    &:first-child {
+      height: 2px;
+      transform: translate(1px, 0px) rotate(0deg);
+      width: 16px;
+    }
+
+    &:nth-child(2) {
+      height: 16px;
+      transform: translateX(-8px) rotate(0deg);
+      width: 2px;
+    }
+  }
+  &:hover {
+    background: rgb(197, 197, 197);
+    transition: 0.2s ease all;
+  }
+`;
+const GroupWatch = styled.div`
+  height: 44px;
+  width: 44px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  background-color: #fff;
+  div {
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background-color: rgb(0, 0, 0);
+
+    img {
+      width: 100%;
+    }
+    &:hover {
+      background: rgb(197, 197, 197);
+      transition: 0.2s ease all;
+    }
+  }
+`;
+const InfoWrapper = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  padding: 16px 0px;
+  color: rgb(249, 249, 249);
+
+  @media screen and (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
 const Details = () => {
   const { id } = useParams();
   const [detailData, setDetailData] = useState({});
@@ -22,9 +203,9 @@ const Details = () => {
             id: doc.id,
             ...movieData,
           });
-          console.log(`Movie Data Fetched }`, movieData);
+          // console.log(`Movie Data Fetched }`, movieData);
         } else {
-          console.log("No Movie Found");
+          // console.log("No Movie Found");
         }
       });
 
@@ -33,188 +214,6 @@ const Details = () => {
 
     return () => unsub(); // Detach the snapshot listener on unmount
   }, []);
-
-  const Container = styled.div`
-    position: relative;
-    min-height: calc(100vh - 250px);
-    overflow-x: hidden;
-    display: block;
-    top: 72px;
-    padding: 0 calc(3.5vw + 5px);
-  `;
-
-  const BackGround = styled.div`
-    left: 0px;
-    opacity: 0.8;
-    position: fixed;
-    top: 0px;
-    right: 0px;
-    width: 100%;
-    z-index: -1;
-    img {
-      width: 100%;
-      height: 100vh;
-      object-fit: cover;
-
-      @media screen and (max-width: 768px) {
-        width: initial;
-      }
-    }
-  `;
-
-  const ImageTitle = styled.div`
-    display: flex;
-    align-items: flex-end;
-    justify-content: flex-start;
-    -webkit-box-pack: start;
-    margin: 0px auto;
-    height: 30vw;
-    min-height: 170px;
-    padding-bottom: 24px;
-    width: 100%;
-
-    img {
-      max-width: 600px;
-      min-width: 200px;
-      width: 35vw;
-    }
-  `;
-  const ContentMeta = styled.div`
-    max-width: 874px;
-  `;
-  const Controls = styled.div`
-    align-items: center;
-    display: flex;
-    flex-flow: row nowrap;
-    margin: 24px 0px;
-    min-height: 56px;
-  `;
-
-  const Player = styled.button`
-    font-size: 15px;
-    padding: 0px 24px;
-    height: 56px;
-    border-radius: 4px;
-    color: rgb(0, 0, 0);
-    border: none;
-    outline: none;
-    cursor: pointer;
-    text-transform: uppercase;
-    background-color: rgba(249, 249, 249);
-    margin: 0px 22px 0px 0px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    letter-spacing: 1.8;
-    text-align: center;
-
-    img {
-      width: 32px;
-    }
-    &:hover {
-      background-color: rgb(198, 198, 198);
-      transition: 0.2s ease all;
-    }
-    @media screen and (max-width: 768px) {
-      height: 45px;
-      padding: 0px 12px;
-      margin: 0px 10px 0px 0px;
-      font-size: 12px;
-
-      img {
-        width: 25px;
-      }
-    }
-  `;
-
-  const Trailer = styled(Player)`
-    background: rgba(0, 0, 0, 0.3);
-    border: 1px solid rgb(249, 249, 249);
-    color: rgb(249, 249, 249);
-    text-transform: uppercase;
-    border-radius: 4px;
-    margin-left: 16px;
-    cursor: pointer;
-  `;
-  const AddList = styled.div`
-    margin-right: 16px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: rgb(249, 249, 249);
-    background: rgba(0, 0, 0, 0.6);
-    font-size: 20px;
-    height: 44px;
-    width: 44px;
-    border-radius: 50%;
-    border: 2px solid rgb(249, 249, 249);
-    cursor: pointer;
-
-    span {
-      background-color: rgba(249, 249, 249);
-      display: inline-block;
-
-      &:first-child {
-        height: 2px;
-        transform: translate(1px, 0px) rotate(0deg);
-        width: 16px;
-      }
-
-      &:nth-child(2) {
-        height: 16px;
-        transform: translateX(-8px) rotate(0deg);
-        width: 2px;
-      }
-    }
-    &:hover {
-      background: rgb(197, 197, 197);
-      transition: 0.2s ease all;
-    }
-  `;
-  const GroupWatch = styled.div`
-    height: 44px;
-    width: 44px;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    background-color: #fff;
-    div {
-      height: 40px;
-      width: 40px;
-      border-radius: 50%;
-      background-color: rgb(0, 0, 0);
-
-      img {
-        width: 100%;
-      }
-      &:hover {
-        background: rgb(197, 197, 197);
-        transition: 0.2s ease all;
-      }
-    }
-  `;
-  const InfoWrapper = styled.div`
-    color: rgb(249, 249, 249);
-    font-size: 15px;
-    min-height: 20px;
-
-    @media screen and (max-width: 768px) {
-      font-size: 12px;
-    }
-  `;
-
-  const Description = styled.div`
-    line-height: 1.4;
-    font-size: 20px;
-    padding: 16px 0px;
-    color: rgb(249, 249, 249);
-
-    @media screen and (max-width: 768px) {
-      font-size: 14px;
-    }
-  `;
 
   return (
     <Container>
