82fe0aa36cc5f62c7370efcfcefdab7539d6c826
lakshya <lakshyaroy848@gmail.com>
2023-08-17 00:09:52 +0530

    trailer section error resolved and css also

--- src/component/Trailer.js
+++ src/component/Trailer.js
@@ -12,28 +12,27 @@ const ContainerWrapper = styled.div`
   justify-content: center;
   align-items: center;
   position: fixed;
-  top: 80px;
+  top: 70px;
   left: 0px;
+  width: 100%;
+  backdrop-filter: blur(5px);
   &::-webkit-scrollbar {
     display: none;
   }
 `;
-const Container = styled.div`
-  min-width: 900px;
-  max-width: 100%;
-  background-color: rgba(15, 16, 20, 0.8);
-  border-radius: 10px;
-  width: 50%;
-  position: relative;
-`;
-const Content = styled.div`
-  height: 450px;
-  width: 800px;
-  padding: 40px;
-  border-radius: 10px;
-  color: white;
-  border-radius: 10px;
-`;
+// const Container = styled.div`
+//   background-color: rgba(15, 16, 20, 0.8);
+//   border-radius: 10px;
+//   width: 50%;
+//   position: relative;
+// `;
+// const Content = styled.div`
+//   height: 450px;
+//   width: 800px;
+//   border-radius: 10px;
+//   color: white;
+//   border-radius: 10px;
+// `;
 const Close = styled.button`
   position: absolute;
   top: 10px;
@@ -44,21 +43,22 @@ const Close = styled.button`
   border: none;
   outline: none;
 `;
-const LinkWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 30px;
-  margin-top: 20px;
-  font-weight: 700;
-  letter-spacing: 1px;
-`;
+// const LinkWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: flex-start;
+//   gap: 30px;
+//   margin-top: 20px;
+//   font-weight: 700;
+//   letter-spacing: 1px;
+// `;
 const MoreMovies = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
   gap: 10px;
+  margin-top: 20px;
   cursor: pointer;
   p {
     font-size: 18px;
@@ -78,6 +78,7 @@ const Cards = styled.div`
   flex-wrap: wrap;
   gap: 20px;
   margin-top: 20px;
+  margin-bottom: 90px;
 
   img {
     border-radius: 10px;
@@ -105,65 +106,87 @@ const Trailers = ({ videoData, setIsTrailer, isTrailer }) => {
   // }, [isTrailer]);
 
   return (
-    <ContainerWrapper>
-      <Container
+    <ContainerWrapper
+      style={{
+        display: isTrailer ? "flex" : "none",
+      }}
+    >
+      {/* <Container> */}
+      {/* <Content> */}
+      <div
         style={{
-          display: isTrailer ? "flex" : "none",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          // justifyContent: "center",
+          height: "100%",
+          overflow: "scroll",
+          width: "50%",
+          backgroundColor: "rgba(15, 16, 20, 0.8)",
+          marginTop: "20px",
+          borderRadius: "5px",
         }}
       >
-        <Content>
-          <Close
-            onClick={() => {
-              setIsTrailer(false);
+        <Close
+          onClick={() => {
+            setIsTrailer(false);
+          }}
+        >
+          <CloseOutlined
+            style={{
+              fontSize: "18px",
+              color: "white",
+              cursor: "pointer",
+              backgroundColor: "red",
+              padding: "4px",
             }}
-          >
-            <CloseOutlined
-              style={{
-                fontSize: "18px",
-                color: "white",
-              }}
-            />
-          </Close>
+          />
+        </Close>
+        <div>
           <iframe
-            width="100%"
-            height="100%"
+            width="765"
+            height="400"
             src={`https://www.youtube-nocookie.com/embed/${videoDetails?.[0]?.key}`}
             title="YouTube video player"
             allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture "
             style={{
               outline: "none",
               border: "none",
-              width: "100%",
-              height: "100%",
               borderRadius: "5px",
               overflow: "hidden",
             }}
           ></iframe>
-          <LinkWrapper>
-            <MoreMovies>
-              <p>More Movies</p>
-              <CardWrapper>
-                <Cards>
-                  {data?.results?.map((movie) => {
-                    return (
-                      <Link to={`/detail/${movie?.id}`}>
-                        <img
-                          src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
-                          alt={movie?.title}
-                          height="225px"
-                          width="150px"
-                        />
-                      </Link>
-                    );
-                  })}
-                </Cards>
-              </CardWrapper>
-            </MoreMovies>
+        </div>
+        {/* <LinkWrapper> */}
+        <MoreMovies>
+          <p>More Movies</p>
+          <CardWrapper>
+            <Cards>
+              {data?.results?.map((movie) => {
+                return (
+                  <Link
+                    to={`/detail/${movie?.id}`}
+                    onClick={() => setIsTrailer(false)}
+                    key={movie?.id}
+                  >
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
+                      alt={movie?.title}
+                      height="225px"
+                      width="150px"
+                    />
+                  </Link>
+                );
+              })}
+            </Cards>
+          </CardWrapper>
+        </MoreMovies>
 
-            {/* <p>Trailers & More</p> */}
-          </LinkWrapper>
-        </Content>
-      </Container>
+        {/* <p>Trailers & More</p> */}
+        {/* </LinkWrapper> */}
+        {/* </Content> */}
+        {/* </Container> */}
+      </div>
     </ContainerWrapper>
   );
 };
